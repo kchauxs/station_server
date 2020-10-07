ebd0fdad426e6581e6cb8b8eae8c7866d0f59911
kevin chaux <k55chasacch@gmail.com>
2020-10-07 16:13:50 -0500

    Correciones en el archivo de configuracion y en el controlador de usuarios

--- components/user/controller.js
+++ components/user/controller.js
@@ -3,13 +3,17 @@ const store = require("./store")
 
 const addUser = (name, email, password) => {
 
-    if (!name || !email || !password){
+    if (!name || !email || !password) {
         return Promise.reject("'Invalid Data User'")
     }
 
+    const user = {
+        name,
+        email,
+        password
+    }
 
-    
-
+    return store.add(user)
 
 }
 
